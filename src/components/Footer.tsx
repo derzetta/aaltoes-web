7451b9db29af28fe74770bc53694f90f07b97344
Daniyar Zharkynuly <daniyar.zharkynuly@aalto.fi>
2025-01-11 02:29:30 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,39 +39,39 @@ function Footer() {
   }, [location.pathname])
 
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-black/30 backdrop-blur-sm border-t border-white/10 py-6 px-6 overflow-x-auto" ref={scrollContainerRef}>
-      <div className="max-w-4xl mx-auto flex flex-nowrap justify-start sm:justify-center gap-8 sm:gap-16 text-sm min-w-max px-4">
-        <Link 
-          to="/" 
-          className={getClassName('/')}
-        >
-          AALTOES 2025
-        </Link>
-        <div className="h-6 w-px bg-white/10" />
-        <Link 
-          to="/billing-info" 
-          className={getClassName('/billing-info')}
-        >
-          Billing
-        </Link>
-        <Link 
-          to="/code-of-conduct" 
-          className={getClassName('/code-of-conduct')}
-        >
-          Code of Conduct
-        </Link>
-        <Link 
-          to="/privacy-notice" 
-          className={getClassName('/privacy-notice')}
-        >
-          Privacy Notice
-        </Link>
-        <Link 
-          to="/association-rules" 
-          className={getClassName('/association-rules')}
-        >
-          Association Rules
-        </Link>
+    <footer className="fixed bottom-0 left-0 right-0 bg-black/30 backdrop-blur-sm border-t border-white/10 py-6">
+      <div className="overflow-x-auto">
+        <div className="max-w-4xl mx-auto flex flex-nowrap justify-start sm:justify-center gap-8 sm:gap-16 text-sm min-w-max px-6">
+          <Link to="/" className={getClassName('/')}>
+            AALTOES 2025
+          </Link>
+          <div className="h-6 w-px bg-white/10" />
+          <Link to="/about" className={getClassName('/about')}>
+            About
+          </Link>
+          <Link to="/board" className={getClassName('/board')}>
+            Board
+          </Link>
+          <Link to="/projects" className={getClassName('/projects')}>
+            Projects
+          </Link>
+          <Link to="/events" className={getClassName('/events')}>
+            Events
+          </Link>
+          <div className="h-6 w-px bg-white/10" />
+          <Link to="/billing-info" className={getClassName('/billing-info')}>
+            Billing
+          </Link>
+          <Link to="/code-of-conduct" className={getClassName('/code-of-conduct')}>
+            Code of Conduct
+          </Link>
+          <Link to="/privacy-notice" className={getClassName('/privacy-notice')}>
+            Privacy Notice
+          </Link>
+          <Link to="/association-rules" className={getClassName('/association-rules')}>
+            Association Rules
+          </Link>
+        </div>
       </div>
     </footer>
   )
