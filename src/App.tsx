5b2ea45c036e966130023cecc590087170a4b27f
Daniyar Zharkynuly <daniyar.zharkynuly@aalto.fi>
2025-01-12 02:27:23 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -241,7 +241,7 @@ function App() {
 
   return (
     <FooterScrollProvider>
-      <div className="relative min-h-screen flex flex-col justify-center overflow-hidden">
+      <div className="relative h-[calc(100vh-96px)] flex flex-col justify-center overflow-hidden">
         <div className="absolute inset-0 bg-black bg-grid-white bg-grid -z-10 sm:h-auto h-[50vh]" />
         <div className="absolute inset-0 bg-gradient-to-b from-black/0 via-black/50 to-black -z-10" />
         <div className="absolute bottom-0 left-0 right-0 h-[50vh] bg-gradient-to-t from-black to-transparent -z-10 sm:hidden" />
