5d996a9748cb4b37ca78d2c1b50fd895c9e0190d
Yerzhan Zhamashev <yerzhan@novel.systems>
2025-04-21 12:03:02 +0300

    Fix Vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { reactRouter } from "@react-router/dev/vite";
-import { vercelPreset } from '@vercel/react-router/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -9,7 +8,6 @@ export default defineConfig(() => {
     base: '/',
     preview: {
       port: 3000
-    },
-    presets: [vercelPreset()],
+    }
   }
 })
